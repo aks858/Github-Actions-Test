7e386bd43176d9d1ad960a0576b67a70885dd465
Amitesh Sharma <amitesh.sharma@Amitesh-Sharma-C02GH2HPMD6R.local>
2022-07-22 09:34:40 -0700

    retry

---
+++ __tests__/appium_config.js
@@ -0,0 +1,9 @@
+const config = {
+    automationName: "XCUITest",
+    platformName: "iOS",
+    platformVersion: "15.2",
+    deviceName: "iPhone 13",
+    app: "/Users/runner/Library/Developer/Xcode/DerivedData/GithubActionsTest-cbzjwynjkoijsiftlxjwlpqksrnj/Build/Products/Debug-iphonesimulator/GithubActionsTest.app"
+};
+
+export {config}
