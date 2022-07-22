84386b4781a69b9af36f5520cf7c4b10bd6060e3
Amitesh Sharma <amitesh.sharma@Amitesh-Sharma-C02GH2HPMD6R.local>
2022-07-22 09:28:43 -0700

    attempting to connect to appium

--- __tests__/appium_config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    automationName: "XCUITest",
-    platformName: "iOS",
-    platformVersion: "15.2",
-    deviceName: "iPhone 13",
-    app: "/Users/runner/Library/Developer/Xcode/DerivedData/GithubActionsTest-cbzjwynjkoijsiftlxjwlpqksrnj/Build/Products/Debug-iphonesimulator/GithubActionsTest.app"
-};
-
-export { config }
