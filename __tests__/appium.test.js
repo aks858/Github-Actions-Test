f2fd9788df71d885981acba9eab5a4a9cdc8387c
Amitesh Sharma <amitesh.sharma@AmiteshH2HPMD6R.attlocal.net>
2022-07-23 21:35:49 -0700

    check if connects to appium

--- __tests__/appium.test.js
+++ __tests__/appium.test.js
@@ -1,29 +1,27 @@
 import wd from 'wd';
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 180000;
-const PORT = 4723;
-const config = {
-    automationName: "XCUITest",
-    platformName: "iOS",
-    platformVersion: "15.2",
-    deviceName: "iPhone 13",
-    app: "/Users/runner/Library/Developer/Xcode/DerivedData/GithubActionsTest-cbzjwynjkoijsiftlxjwlpqksrnj/Build/Products/Debug-iphonesimulator/GithubActionsTest.app"
-};
-const driver = wd.promiseChainRemote('localhost', PORT);
-
-
 try {
-    // beforeAll(async () => {
+    beforeAll(async () => {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+        const PORT = 4723;
+        const config = {
+            automationName: "XCUITest",
+            platformName: "iOS",
+            platformVersion: "15.2",
+            deviceName: "iPhone 13",
+            app: "/Users/runner/Library/Developer/Xcode/DerivedData/GithubActionsTest-cbzjwynjkoijsiftlxjwlpqksrnj/Build/Products/Debug-iphonesimulator/GithubActionsTest.app"
+        };
+        const driver = wd.promiseChainRemote('localhost', PORT);
 
-    //     const data = await driver.status();
-    //     console.log(data);    
-    //     let pageSource = await driver.source();
-    //     console.log(pageSource);
+        const data = await driver.status();
+        console.log(data);    
+        let pageSource = await driver.source();
+        console.log(pageSource);
       
         
-    //     await driver.init(config);
-    //     await driver.sleep(2000); // wait for app to load a
-    //   })
+        await driver.init(config);
+        await driver.sleep(2000); // wait for app to load a
+      })
       
       test('appium renders', async () => {
         // expect(await driver.hasElementByAccessibilityId('app-view')).toBe(true);
