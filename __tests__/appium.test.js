cdaaff922c7082af4242b42425233bf36c4ead88
Amitesh Sharma <amitesh.sharma@Amitesh-Sharma-C02GH2HPMD6R.local>
2022-07-25 14:04:29 -0700

    testing self runner pt2

--- __tests__/appium.test.js
+++ __tests__/appium.test.js
@@ -2,37 +2,21 @@ import wd from 'wd';
 
 const PORT = 4723;
 const driver = wd.promiseChainRemote('localhost', PORT);
-try {
-    beforeAll(async () => {
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
-        const config = {
-            platformName: "iOS",
-            platformVersion: "15.2",
-            deviceName: "iPhone 13",
-            app: "/Users/runner/Library/Developer/Xcode/DerivedData/GithubActionsTest-cbzjwynjkoijsiftlxjwlpqksrnj/Build/Products/Debug-iphonesimulator/GithubActionsTest.app"
-        };
-        console.log(config );
-        try {
-            await driver.init(config);
-            console.log(await driver);
-        } catch(e) {
-            console.log(e);
-        }
-        
-        // await driver.init(config);
-        // await driver.sleep(2000); // wait for app to load a
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+const config = {
+    platformName: "iOS",
+    platformVersion: "15.5",
+    deviceName: "iPhone 13",
+    app: "/Users/runner/Library/Developer/Xcode/DerivedData/GithubActionsTest-cbzjwynjkoijsiftlxjwlpqksrnj/Build/Products/Debug-iphonesimulator/GithubActionsTest.app"
+};
 
-        // const data = await driver.status();
-        // console.log(data);    
-        // let pageSource = await driver.source();
-        // console.log(pageSource);
-      })
-      
-      test('appium renders', async () => {
-        // expect(await driver.hasElementByAccessibilityId('app-view')).toBe(true);
-        // expect(await driver.hasElementByAccessibilityId('app-view-not-here')).toBe(false);
-        expect(true).toBe(true);
-      });
-} catch {
-    console.log("swift error handling")
-}
+beforeAll(async () => {
+    await driver.init(config);
+    await driver.sleep(2000); // wait for app to load a
+})
+    
+test('appium renders', async () => {
+    expect(await driver.hasElementByAccessibilityId('app-view')).toBe(true);
+    expect(await driver.hasElementByAccessibilityId('app-view-not-here')).toBe(false);
+    expect(true).toBe(true);
+});
